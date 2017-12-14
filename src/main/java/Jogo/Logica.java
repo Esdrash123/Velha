0ebd493f3a6abfe435b5d21c702dbd94b19b3300
Esdras Henrique <esdrasha1@gmail.com>
2017-12-14 10:16:13 -0300

    Inicio do jogo

--- src/main/java/Jogo/Logica.java
+++ src/main/java/Jogo/Logica.java
@@ -14,18 +14,88 @@ public class Logica {
     public Logica() {
     }
 
-    String[][] m = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+    String[][] matriz = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
 
     public String tabuleiro() {
 
-        String Posicao = "";
+        String casa = "";
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                Posicao = Posicao + "  " + (m[i][j]);
+                casa = casa + "  " + (matriz[i][j]);
             }
-            Posicao = Posicao + "\n";
+            casa = casa + "\n";
         }
-        return Posicao;
+        return casa;
+    }
+
+    public String realizarJogada(String posicao, String j) {
+        String l = "Jogada Realizada";
+
+        if (posicao.equals("1")) {
+            matriz[0][0] = j;
+        } else if (posicao.equals("2")) {
+            matriz[0][1] = j;
+        } else if (posicao.equals("3")) {
+            matriz[0][2] = j;
+        } else if (posicao.equals("4")) {
+            matriz[1][0] = j;
+        } else if (posicao.equals("5")) {
+            matriz[1][1] = j;
+        } else if (posicao.equals("6")) {
+            matriz[1][2] = j;
+        } else if (posicao.equals("7")) {
+            matriz[2][0] = j;
+        } else if (posicao.equals("8")) {
+            matriz[2][1] = j;
+        } else if (posicao.equals("9")) {
+            matriz[2][2] = j;
+        } else if (!posicao.equals("9") && !posicao.equals("8") && !posicao.equals("7") && !posicao.equals("6")
+                && !posicao.equals("5") && !posicao.equals("4") && !posicao.equals("3") && !posicao.equals("2")
+                && !posicao.equals("1")) {
+            l = "número inválido";
+        }
+        return l;
+    }
+
+    public boolean validarJogada(String posicao) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matriz[i][j].equals(posicao)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public String Ganhou(int jogadas) {
+
+        String[] T = new String[8];
+        String vencedor = "null";
+
+        if (jogadas == 9) {
+            vencedor = "OPS, deu velha ngm";
+        }
+        //linha
+        T[0] = matriz[0][0] + matriz[0][1] + matriz[0][2];
+        T[1] = matriz[1][0] + matriz[1][1] + matriz[1][2];
+        T[2] = matriz[2][0] + matriz[2][1] + matriz[2][2];
+        //coluna
+        T[3] = matriz[0][0] + matriz[1][0] + matriz[2][0];
+        T[4] = matriz[0][1] + matriz[1][1] + matriz[2][1];
+        T[5] = matriz[0][2] + matriz[1][2] + matriz[2][2];
+        //Diagonal
+        T[6] = matriz[0][0] + matriz[1][1] + matriz[2][2];
+        T[7] = matriz[0][2] + matriz[1][1] + matriz[2][0];
+
+        for (int i = 0; i < T.length; i++) {
+            if (T[i].equals("xxx")) {
+                vencedor = " Jogador 1";
+            } else if (T[i].equals("000")) {
+                vencedor = " Jogador 2";
+            }
+        }
+        return vencedor;
     }
 
 }
